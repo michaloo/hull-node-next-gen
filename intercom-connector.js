de8b01851815b621b5c75849c77da189fe0c2f3a
Michal Raczka <michal@raczka.me>
2018-05-21 16:59:34 +0200

    More docs

--- intercom-connector.js
+++ intercom-connector.js
@@ -11,46 +11,70 @@ class IntercomConnector extends HullConnectorNextGen {
    */
   handleUserUpdateMessages(ctx, messages, options = {}) {
 
-    return Promise.resolve();
+    return Promise.resolve({
+      logs: [],
+      firehoseEvents: []
+    });
   }
 
   /**
    * run on account update notifications and batch
    */
   handleAccountUpdateMessages(ctx, messages, options) {
-
+    return Promise.resolve({
+      logs: [],
+      firehoseEvents: []
+    });
   }
 
   handleSegmentUpdateMessages(ctx) {
-
+    return Promise.resolve({
+      logs: [],
+      firehoseEvents: []
+    });
   }
 
   handleStatusScheduler(ctx) {
-
+    return Promise.resolve({
+      logs: [],
+      firehoseEvents: []
+    });
   }
 
   handleFetchScheduler(ctx) {
-
+    return Promise.resolve({
+      logs: [],
+      firehoseEvents: []
+    });
   }
 
   handleFetchLeadsScheduler(ctx) {
-
+    return Promise.resolve({
+      logs: [],
+      firehoseEvents: []
+    });
   }
 
   handleFetchSegmentsScheduler(ctx) {
-
+    return Promise.resolve({
+      logs: [],
+      firehoseEvents: []
+    });
   }
 
   handleIncomingWebhook(ctx) {
-
+    return Promise.resolve({
+      logs: [],
+      firehoseEvents: []
+    });
   }
 
   loadUserFields(ctx) {
-
+    return Promise.resolve(jsonResponse);
   }
 
   handleAdmin(ctx) {
-
+    return Promise.resolve();
   }
 }
 
